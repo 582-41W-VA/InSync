54a1dc4e34c819fc5fc140f173511ece8db490a5
Nupur Upadhyaya <upadhyayanupur3@gmail.com>
2025-01-23 16:50:31 -0500

    insync_platform folder created

--- insync_platform/settings.py
+++ insync_platform/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'insync_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "insync_platform/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
