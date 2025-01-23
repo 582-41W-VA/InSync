7a71b36df0d305e300aa2685f2a8f40fa209ebc3
Nupur Upadhyaya <upadhyayanupur3@gmail.com>
2025-01-23 11:22:25 -0500

    added path for home

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "account"
 urlpatterns = [
+    path("", views.home, name="home"), 
     path("", views.index, name="index"),
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
