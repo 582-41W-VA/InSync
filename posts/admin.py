5df85971174a2146b6b6658d39acc499fa237c6c
Mohame <wtafroach@gmail.com>
2025-02-12 12:37:41 -0500

    added more fields for categorizing data ex. flag_count, like_count

--- posts/admin.py
+++ posts/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 from .models import Post, Comment, Tag, Upvote, Flag, Media, Save
+from django.db.models import Count
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'post', 'user', 'created_at', 'updated_at', 'text', 'upvote_count')
-    list_filter = ('created_at',)
-    search_fields = ('text', 'user', 'text',)
+    list_display = ('id', 'post', 'user', 'created_at', 'updated_at', 'text', 'upvote_count', 'flag_count')
+    list_filter = ('created_at', 'updated_at',)
+    search_fields = ('text', 'user')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(upvote_count=Count('comment_upvotes'))
+        return queryset
+
+    def upvote_count(self, obj):
+        return obj.upvote_count
+    upvote_count.admin_order_field = 'upvote_count'
 
 class FlagAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'post', 'comment', 'reason', 'flagged_at')
-    list_filter = ('flagged_at',)
+    list_filter = ('flagged_at', 'reason')
     search_fields = ('reason', 'user')
 
 class UpvoteAdmin(admin.ModelAdmin):
@@ -17,9 +27,19 @@ class UpvoteAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'post__title', 'comment__text')
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'created_at', 'updated_at', 'upvote_count', 'top_level_comments_count', 'total_comments')
+    list_display = ('id', 'title', 'user', 'created_at', 'updated_at', 'upvote_count', 'top_level_comments_count', 'total_comments', 'flag_count')
     list_filter = ('tags', 'created_at',)
     search_fields = ('title', 'content')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(upvote_count=Count('post_upvotes'))
+        return queryset
+
+    def upvote_count(self, obj):
+        return obj.upvote_count
+    upvote_count.admin_order_field = 'upvote_count'  
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
